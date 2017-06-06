14ef9704a3c00b38f30a246d3deb59d25c63c4c4
Spicer Bak <Spicer.Bak@usace.army.mil>
2017-06-06 13:34:32 -0400

    changed keys around and updated sub modules to work with new FRF coord function

--- MakeTodaysGridSTWAVE.py
+++ MakeTodaysGridSTWAVE.py
@@ -27,43 +27,41 @@ makenc.makenc_todaysBathyCMTB(yesterdaysBathyPacket, yesterdaysListfname[0], glo
 ##################################################################
 # # # ## now loop through history on thredds #####################
 ##################################################################
-methods = ['matplotlib'] # plant, metpy, and matplotlib also valid
-for interpType in methods:
-    newncfile = nc.Dataset('http://bones/thredds/dodsC/FRF/survey/gridded/gridded.ncml')
-    for idxNew, time in enumerate(newncfile['time'][:]):
-        date = nc.num2date(time, newncfile['time'].units)  # date for the new survey
-        # remove fill values from newNCfile
-        newXfrf = newncfile['xFRF'][:]
-        newYfrf = newncfile['yFRF'][:]
-        newZfrf = newncfile['elevation'][idxNew]
-        if type(newZfrf) == np.ma.MaskedArray:
-            # now remove masked data based on Z from the x and y coords
-            newXfrf = newXfrf[~np.all(newZfrf.mask, axis=0)]
-            newYfrf = newYfrf[~np.all(newZfrf.mask, axis=1)]
-            newZfrf = newZfrf[~np.all(newZfrf.mask, axis=1), :]  #
-            newZfrf = newZfrf[:, ~np.all(newZfrf.mask, axis=0)]
+newncfile = nc.Dataset('http://bones/thredds/dodsC/FRF/survey/gridded/gridded.ncml')
+for idxNew, time in enumerate(newncfile['time'][:]):
+    date = nc.num2date(time, newncfile['time'].units)  # date for the new survey
+    # remove fill values from newNCfile
+    newXfrf = newncfile['xFRF'][:]
+    newYfrf = newncfile['yFRF'][:]
+    newZfrf = newncfile['elevation'][idxNew]
+    if type(newZfrf) == np.ma.MaskedArray:
+        # now remove masked data based on Z from the x and y coords
+        newXfrf = newXfrf[~np.all(newZfrf.mask, axis=0)]
+        newYfrf = newYfrf[~np.all(newZfrf.mask, axis=1)]
+        newZfrf = newZfrf[~np.all(newZfrf.mask, axis=1), :]  #
+        newZfrf = newZfrf[:, ~np.all(newZfrf.mask, axis=0)]
 
-        backgroundGridnc = nc.Dataset(yesterdaysListfname[-1])  # updating to the background grid
-        idxOld = 0   #len(yesterdaysListfname)-1  # counts upwards with each one made
+    backgroundGridnc = nc.Dataset(yesterdaysListfname[-1])  # updating to the background grid
+    idxOld = 0   #len(yesterdaysListfname)-1  # counts upwards with each one made
 
-        # make data packet
-        dataPacket = {'time': date,
-                      'newZfrf': newZfrf,
-                      'newYfrf': newYfrf,
-                      'newXfrf': newXfrf,
-                      'oldBathy': backgroundGridnc['elevation'][idxOld],
-                      'modelGridX': backgroundGridnc['xFRF'][:],
-                      'modelGridY': backgroundGridnc['yFRF'][:],
-                      'latitude': backgroundGridnc['latitude'][:],
-                      'longitude': backgroundGridnc['longitude'][:],
-                      'easting': backgroundGridnc['easting'][:],
-                      'northing': backgroundGridnc['northing'][:],
-                      'azimuth': backgroundGridnc['azimuth'][:],
-                      'x0': backgroundGridnc['x0'][:],
-                      'y0':backgroundGridnc['y0'][:],
-                      }
-
-        print ' Working on %s interpolation now for %s' % (interpType, date)
-        ofnameNC = dataLocation + '/todaysBathyNewFromGrids_%s_STWAVE_%sm.nc' % (date.strftime('%Y-%m-%d'), resolution)
-        gridTools.MakeTodaysBathy(ofnameNC=ofnameNC, dataPacket=dataPacket, plotFlag=True, interpType= interpType)
-        yesterdaysListfname.append(ofnameNC)
+    # make data packet
+    dataPacket = {'time': date,
+                  'newZfrf': newZfrf,
+                  'newYfrf': newYfrf,
+                  'newXfrf': newXfrf,
+                  'oldBathy': backgroundGridnc['elevation'][idxOld],
+                  'modelGridX': backgroundGridnc['xFRF'][:],
+                  'modelGridY': backgroundGridnc['yFRF'][:],
+                  'latitude': backgroundGridnc['latitude'][:],
+                  'longitude': backgroundGridnc['longitude'][:],
+                  'easting': backgroundGridnc['easting'][:],
+                  'northing': backgroundGridnc['northing'][:],
+                  'azimuth': backgroundGridnc['azimuth'][:],
+                  'x0': backgroundGridnc['x0'][:],
+                  'y0':backgroundGridnc['y0'][:],
+                  }
+    interpType = 'matplotlib'
+    print ' Working on interpolation now for %s' % (date)
+    ofnameNC = dataLocation + '/todaysBathyNewFromGrids_%s_STWAVE_%sm.nc' % (date.strftime('%Y-%m-%d'), resolution)
+    gridTools.MakeTodaysBathy(ofnameNC=ofnameNC, dataPacket=dataPacket, plotFlag=True, interpType= interpType)
+    yesterdaysListfname.append(ofnameNC)
